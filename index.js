8c4040e38ad025a87a6dd72a04b50c10495952be
Bob Carver <bob.m.carver@gmail.com>
2021-06-06 23:30:20 -0500

    html/svg working

--- index.js
+++ index.js
@@ -1,81 +1,100 @@
-
-const model = {
-    floor: Array(10).fill([]),
-    active: {orient: 0, x: 5,y: 21, shape: undefined},
-
-    move : 
+shapes = {
+    'I': {blocks:[[-2,-1], [-1,-1], [0, -1], [1,-1]], spawn:0},
+    'O': {blocks:[[-1,-1], [0,-1],  [-1, 0], [0, 0]], spawn:0},
+    'T': {blocks:[[-1, 0], [ 0, 0], [1,  0], [0,-1]], spawn:0},
+    'S': {blocks:[[-2,-1], [-1,-1], [0, -1], [1,-1]], spawn:0},
+    'Z': {blocks:[[-2,-1], [-1,-1], [0, -1], [1,-1]], spawn:0},
+    'J': {blocks:[[-2,-1], [-1,-1], [0, -1], [1,-1]], spawn:0},
+    'L': {blocks:[[-2,-1], [-1,-1], [0, -1], [1,-1]], spawn:0}
 }
-move(rotation, xy){
 
+let holdAvail = true, transform, timer
+function startGame() {
+    sequence =
+    for(7) sequence
+    nextPiece()
+    timer = setInterval()
 }
-document.addEventListener('keydown', event => {
-    event.code
-    case 'ArrowRight': 
-    if( MSF)
 
-    key
+sequence =sequence.slice(0,7)+ getSelection.next()
+
+function nextPiece(){
+    transform = [1,0,0,0,1,1]
+    sequence.forEach( (c,i) => windows[`shape${i}`].href = `#${c}`)
+}
+
+function endGame() {
+    clearInterval(timer)
+    alert("Game Over")
+    document.removeEventListener('keyup', x)
+    document.removeEventListener('keypress', y)
+}
+ document.addEventListener('keyup', event => {
+    if(event.code == 'ArrowDown')// end soft drop
+    clearInterval(timer)
+    timer = setInterval(tick, model.interval)
+    // adjust tick
+ })
+
+document.addEventListener('keypress', event => {
+
+    switch (event.code) {
+        case 'ArrowDown':// soft drop
+            clearInterval(timer)
+            timer = setInterval(tick, model.interval/2)
+            return;
+
+        case ' ':// hard drop
+            transform = model.hardDrop(theShape, transform)
+            return
+
+        case 'C': case 'Shift':// hold
+            if( holdAvail ){
+                holdAvail = false
+                transform = [1,0,0,0,1,0]
+                [shape0.href, hold.href] = [hold.href, theShape.shape0]
+                shape0.href != '' || nextBlock()
+            }// reset holdAvail in finalize
+            return
+        // the following cases are for movement of
+        // the shape.  They all set a transform that is
+        // used after the switch
+        // right
+        case 'ArrowRight': M = [a,b,c+1,d,e,f]; break;
+        //left
+        case 'ArrowLeft': M =[a,b,c-1,d,e,f]; break;
+        // rotate clockwise
+        case 'ArrowUp': case 'X': M = [-b,a,c,e,d,f]; break;
+        // rotate counter-clockwise
+        case 'Ctrl': case 'Z':M = [b,a,c,e,-d,f];
+    }
+    if(model.check(theShape, M)) { // ?? twist and kicks
+        transform = M
+        shape0.transform = `M`
+    }
 })
+
 function tick(){
-    if( model.move(down)) shape.y += 1
-    else delay(500).then( () => {
-        rows = freeze()
-        removeRows(rows)
-
-        for( const b of theFloor.querySelectorAll('rect')) {
-            if(row.includes(b.y)) theFloor.removeChild(b)
-            else {
-                b.y += rows.reduce((acc,e)=> acc + b.y > e)
-        }
-        // score ?
-        if( ! spawn() )gameOver()
-    })
+    transform[5] += 1
+    if( !model.check(theShape, transform)) {
+        delay(500).then(finalize)
+    } else shape0.y += 1
 }
 
-function newBlock() {
-    active = game.createElement('g')
-    active.innerHTML =
-    block.blocks.map( ([x,y]) =>
-        `<rect x="${x}" y="${y}" class="${name}" />`
-    ).join()
+function finalize() {
+    holdAvail = true
+    clearInterval(timer)
+    const rows = model.freeze(theShape, transform)
+    // update UI
+    theFloor.lastElementChild.outerHTML += // is x,y correct ?
+    svg[c].blocks.map(([x,y]) => `<rect x=${x} y="${y} fill="${svg[c].fill}" />`)
+    for( const b of theFloor.querySelectorAll('rect'))
+        if(row.includes(b.y)) theFloor.removeChild(b) // remove full lines
+        else b.y += rows.reduce((acc,e)=> acc + b.y > e) // move remaining down
+    // update score, level & lines
+    ['score','level','lines','pieces'].forEach(l =>
+        window[i].textContent = model[l]
+    )
+    // adjust tick
+    nextPiece()
 }
-
-
-
-function computeN(){
-    return min(...realPos().map(([x,y]) => floor[x].length - y))
-}
-
-score = 40 * (n + 1)	100 * (n + 1)	300 * (n + 1)	1200 * (n + 1)
-
-setInterval(() => {
-    if( drop != 0 ) y++
-    else {
-        delay(500).then(()=>
-            freeze pieces
-            active = next
-            new next
-            drop = check(0,0,0)
-            if( drop == 0 )endGame()
-
-        )
-    }
-    if( no active) makepiece || endgame
-    movepiece down
-    check grounded
-}, 1);
-
- check = () => realPos().every(([x,y]) => !board[x]?.[y])
-
-    function movex(n){
-        if check(n){
-            active.x += n
-            active.svg.x += n;
-            update()
-        }
-    }
-    function rotate(n){
-        check() {
-            active.svg.transform =
-            update()
-        }
-    }
--- model.mjs
+++ model.mjs
@@ -1,38 +1,31 @@
-blocks = {
-    'I': {blocks:[[-2,-1], [-1,-1], [0, -1], [1,-1]], spawn:0},
-    'O': {blocks:[[-1,-1], [0,-1],  [-1, 0], [0, 0]], spawn:0},
-    'T': {blocks:[[-1, 0], [ 0, 0], [1,  0], [0,-1]], spawn:0},
-    'S': {blocks:[[-2,-1], [-1,-1], [0, -1], [1,-1]], spawn:0},
-    'Z': {blocks:[[-2,-1], [-1,-1], [0, -1], [1,-1]], spawn:0},
-    'J': {blocks:[[-2,-1], [-1,-1], [0, -1], [1,-1]], spawn:0},
-    'L': {blocks:[[-2,-1], [-1,-1], [0, -1], [1,-1]], spawn:0}
-}
+
 let lines = 0, level = 0
 const shapeQue = []
 function* next() {
-    if( shapeQue.length < 6)
-        shapeQue.concat("IOTSZJL".split().map((e,i,a) => {
-        const n =(a.length-i)*random(),
-        t = a[n]
-        a[n] = a[i]
-        a[i] = t
-    }))
-    yield shapeQue.shift() 
+    while(t){
+        "IOTSZJL".split().forEach((e,i,a) => {
+        const n =(a.length-i)*random()
+        yield a[n]
+        a[n]= e
+    })}
 }
 
-freeze = () =>{
+freeze = (shapes, [a,b,c,d,e,f]) =>{
+
     let ymax = 0, ymin= 20
-    for( const [x,y] of shape.blocks) {
-        if(rotation|1) [y,x]=[x,y]
-        x = active.x + rotation | 2 ? -x : x
-        y = active.y + rotation ^ 3 ? -y : y
-        ymin = min(y, ymin)
-        ymax = max(y, ymax)
-        floor[x][y] = true
+    for( const [x,y] of shapes) {
+        const
+        xp = a*x + b*y + c,
+        yp = d*x + e*y + f
+        ymin = min(yp, ymin)
+        ymax = max(yp, ymax)
+        floor[xp][yp] = true
     }
     const rows = floor.slice(ymin, ymax+1).filter( row => floor.every(col => col[row]))
     lines += rows.length
     level = line / 10 | 0
+    //score = 40 * (n + 1)	100 * (n + 1)	300 * (n + 1)	1200 * (n + 1)
+
     // update score, lines etc
     for( const row of rows) {
         for( const col of floor) {
@@ -45,13 +38,12 @@ freeze = () =>{
         while(!col.at(-1) && col.length)col.length--
     return rows
 
-}
+},
 
-function check(){
-    active.shape.blocks.every(([x,y]) => {
-        if(rotation|1) [y,x]=[x,y]
-        x = active.x + rotation | 2 ? -x : x
-        y = active.y + rotation ^ 3 ? -y : y
-        y>= 0 && floor[x] &&  floor[x][y]
+check = (shapes, [a,b,c,d, e, f]) => {
+    shapes.every(([x,y]) => {
+        const xp = a*x +b*y + c,
+        yp = d*x +e*y + f
+        yp>= 0 && floor[xp] &&  floor[xp][yp]
     })
 }
